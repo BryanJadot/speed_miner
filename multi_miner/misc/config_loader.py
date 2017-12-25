4cc7002a6b8c4625b45172fb9002e3c16ec8ba4e
Bryan Jadot <bryan.jadot@gmail.com>
2017-12-25 13:14:53 -0800

    Add default_parse_ and better error checking.

--- multi_miner/misc/config_loader.py
+++ multi_miner/misc/config_loader.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 
 from json import loads
 from json.decoder import JSONDecodeError
@@ -37,7 +37,35 @@ def _catch_invalid_config(func):
     return wrapper
 
 
-class MiningConfigLoader(ABC):
+class MiningConfigLoaderMeta(ABCMeta):
+
+    def __init__(cls, name, bases, dct):
+        super(MiningConfigLoaderMeta, cls).__init__(name, bases, dct)
+        desc_funcs = cls._get_prefix_funcs("describe_")
+        parse_funcs = cls._get_prefix_funcs("parse_")
+        default_parse_funcs = cls._get_prefix_funcs("default_parse_")
+
+        descs = set(desc_funcs.keys())
+        parses = set(parse_funcs.keys()) | set(default_parse_funcs.keys())
+
+        assert descs - parses == set(), "Missing parse function(s) for params: %s" % ", ".join(descs - parses)
+        assert parses - descs == set(), "Missing describe function(s) for params: %s" % ", ".join(parses - descs)
+
+
+class MiningConfigLoader(ABC, metaclass=MiningConfigLoaderMeta):
+
+    @classmethod
+    def _get_prefix_funcs(cls, prefix):
+        # Get a map dict where the values are functions starting with a specified prefix
+        # and the keys are the names of the functions, with the prefix snipped off.
+        #
+        # Ex: if a class contains the functions 'parse_group' and 'parse_coin', this will
+        # return {'group': parse_group, 'coin': parse_coin}.
+        return {
+            func[len(prefix):]: getattr(cls, func) for func in dir(cls)
+            if callable(getattr(cls, func)) and func.startswith(prefix)
+        }
+
     @_catch_invalid_config
     def process_config_from_file(self, filename):
         try:
@@ -47,17 +75,6 @@ class MiningConfigLoader(ABC):
 
         return self.process_config_from_dict(json_dict)
 
-    def _get_prefix_funcs(self, prefix):
-        # Get a map dict where the values are functions starting with a specified prefix
-        # and the keys are the names of the functions, with the prefix snipped off.
-        #
-        # Ex: if a class contains the functions 'parse_group' and 'parse_coin', this will
-        # return {'group': parse_group, 'coin': parse_coin}.
-        return {
-            func[len(prefix):]: getattr(self, func) for func in dir(self)
-            if callable(getattr(self, func)) and func.startswith(prefix)
-        }
-
     @_catch_invalid_config
     def process_config_from_dict(self, json_dict):
         parse_funcs = self._get_prefix_funcs("parse_")
@@ -65,20 +82,35 @@ class MiningConfigLoader(ABC):
         if len(missing_keys) > 0:
             raise InvalidMiningConfig("Missing required config parameter(s) - %s" % ", ".join(missing_keys))
 
+        parse_funcs.update(self._get_prefix_funcs("default_parse_"))
         return {
-            k: func(json_dict[k])
+            k: func(self, json_dict.get(k))
             for k, func in parse_funcs.items()
         }
 
     def _print_error(self, err):
         pr("\033[31mError occurred: \"%s\".\033[m\n\n" % str(err), stream=stderr)
-
         pr("%s\nPossible parameters:\n" % self.describe(), stream=stderr)
 
-        cmd_to_description = self._get_prefix_funcs("describe_")
-        for cmd, desc_func in cmd_to_description.items():
-            string = "\n\033[1m%s\033[0m\n\t%s\n" % (cmd, desc_func().replace("\n", "\n\t"))
+        parse_params = sorted(list(self._get_prefix_funcs("parse_").keys()))
+        default_funcs = self._get_prefix_funcs("default_parse_")
+        default_parse_params = sorted(list(default_funcs.keys()))
+
+        param_to_description = self._get_prefix_funcs("describe_")
+        for param in parse_params:
+            desc_func = param_to_description[param]
+            string = "\n\033[1m%s\033[0m\n\t%s\n" % (param, desc_func(self).replace("\n", "\n\t"))
             pr(string, stream=stderr)
+
+        for param in default_parse_params:
+            desc_func = param_to_description[param]
+            string = "\n\033[1m%s (default: \"%s\")\033[0m\n\t%s\n" % (
+                param,
+                default_funcs[param](self, None),
+                desc_func(self).replace("\n", "\n\t"),
+            )
+            pr(string, stream=stderr)
+
         pr("\n", stream=stderr)
 
     @abstractmethod
