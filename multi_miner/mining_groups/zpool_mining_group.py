ac71b90b81bc55fcb1e1b6374198b82e2faffacb
Bryan Jadot <bryan.jadot@gmail.com>
2017-12-26 09:52:46 -0800

    Introduce new source of prof info

--- multi_miner/mining_groups/zpool_mining_group.py
+++ multi_miner/mining_groups/zpool_mining_group.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from multi_miner.miners.ccminer import CCMiner
 from multi_miner.mining_groups.abstract_mining_group import AbstractMiningGroup
 from multi_miner.misc.config_loader import InvalidMiningConfig, MiningConfigLoader
@@ -18,6 +20,41 @@ class ZPoolMiningGroupLoader(MiningConfigLoader):
         else:
             raise InvalidMiningConfig("This is probably a bad payout currency. Should be three letters.")
 
+    def describe_algo_data_source(self):
+        desc = "Put the desired zpool API with which to calculate profitability. Currently, it doesn't seem one API is best for maximizing profitability and there are only small differences in values between them. Possible values:"
+        desc += "* \"algo\": This will query the API that gives estimates and data bucketed by algo. Currently, this seems to be the most stable API."
+        desc += "* \"currency\": This will query the API that gives estimates and data bucketed by currency. Currently, a less stable API."
+        return desc
+
+    def default_parse_algo_data_source(self, value):
+        if value is None:
+            return "algo"
+
+        possible_values = {"algo", "currency"}
+
+        if value not in possible_values:
+            raise InvalidMiningConfig("%s is a bad algo data source." % value)
+
+        return value
+
+    def describe_calc_all_algo_data_sources(self):
+        return "This can be \"true\" or \"false\". If \"true\", then we will calculate profitability from all data sources and print them out. This will not affect which data source gets used, this is merely to allow comparisons of different data sources."
+
+    def default_parse_calc_all_algo_data_sources(self, value):
+        if value is None:
+            return False
+
+        possible_values = {"true", "false"}
+
+        value = value.lower().strip()
+        if value not in possible_values:
+            raise InvalidMiningConfig("%s is a bad value for \"calc_all_data_source\"." % value)
+
+        return value == "true"
+
+
+AlgoInfo = namedtuple("AlgoInfo", ["port", "algo", "prof_rate"])
+
 
 class ZPoolMiningGroup(AbstractMiningGroup):
     ZPOOL_URL_SUFFIX = ".mine.zpool.ca"
@@ -29,11 +66,18 @@ class ZPoolMiningGroup(AbstractMiningGroup):
 
     @staticmethod
     def init_from_config(config):
-        return ZPoolMiningGroup(config["payout_currency"], config["wallet"])
+        return ZPoolMiningGroup(
+            config["payout_currency"],
+            config["wallet"],
+            algo_data_source=config["algo_data_source"],
+            calc_all_algo_data_sources=config["calc_all_algo_data_sources"],
+        )
 
-    def __init__(self, payout_currency, wallet):
+    def __init__(self, payout_currency, wallet, algo_data_source="status", calc_all_algo_data_sources=False):
         self._payout_currency = payout_currency
         self._wallet = wallet
+        self._algo_data_source = algo_data_source
+        self._calc_all_algo_data_sources = calc_all_algo_data_sources
 
     def _get_zpool_profitability_unit(self, algo):
         if algo == "sha256":
@@ -107,39 +151,57 @@ class ZPoolMiningGroup(AbstractMiningGroup):
         else:
             raise Exception("Unsupported algo: %s" % algo)
 
-    def _get_currencies(self):
+    def _currency_to_algo_info(self, currencies):
+        currencies = sorted(currencies, key=lambda c: float(c["estimate"]), reverse=True)
+        algo_info_dict = {}
+
+        for c in currencies:
+            if c["algo"] not in algo_info_dict:
+                algo_info_dict[c["algo"]] = AlgoInfo(algo=c["algo"], prof_rate=float(c["estimate"]), port=c["port"])
+
+        return list(algo_info_dict.values())
+
+    def _get_algo_info_from_currencies(self):
         LOG.debug("Fetching currency info...")
-        return list(Fetcher.fetch_json_api("http://www.zpool.ca/api/currencies").values())
+        return self._currency_to_algo_info(list(Fetcher.fetch_json_api("http://www.zpool.ca/api/currencies").values()))
 
-    def _fetch_most_profitable_algo(self, currencies, algo_to_benchmarks):
-        LOG.debug("Finding most profitable algo...")
+    def _status_to_algo_info(self, statuses):
+        return [
+            AlgoInfo(algo=s["name"], prof_rate=float(s["estimate_current"]) * 1000.0, port=s["port"])
+            for s in statuses
+        ]
 
-        def _get_prof(c):
-            return algo_to_benchmarks[c["algo"]].get_24_hour_profitability(
-                float(c["estimate"]),
-                self._get_zpool_profitability_unit(c["algo"]),
+    def _get_algo_info_from_statuses(self):
+        LOG.debug("Fetching algo statuses...")
+        return self._status_to_algo_info(list(Fetcher.fetch_json_api("http://www.zpool.ca/api/status").values()))
+
+    def _fetch_most_profitable_algo(self, algo_info, algo_to_benchmarks):
+        def _get_prof(a):
+            return algo_to_benchmarks[a.algo].get_24_hour_profitability(
+                a.prof_rate,
+                self._get_zpool_profitability_unit(a.algo),
             )
 
-        sorted_currencies = sorted(
-            [c for c in currencies if c["algo"] in algo_to_benchmarks],
+        sorted_algo_info = sorted(
+            [a for a in algo_info if a.algo in algo_to_benchmarks],
             key=_get_prof,
             reverse=True,
         )
-        for v in sorted_currencies:
-            LOG.debug("    Profitability of %s (%s) = %0.4f", v["name"], v["algo"], _get_prof(v))
+        for a in sorted_algo_info:
+            LOG.debug("    Profitability of %s = %0.4f", a.algo, _get_prof(a))
 
-        return sorted_currencies[0]["algo"]
+        return sorted_algo_info[0].algo
 
-    def _filter_blacklisted_algos_from_currencies(self, currencies):
-        return [c for c in currencies if c["algo"] not in ZPoolMiningGroup.BLACKLISTED_ALGOS]
+    def _filter_blacklisted_algos_from_algo_info(self, algo_info):
+        return [a for a in algo_info if a.algo not in ZPoolMiningGroup.BLACKLISTED_ALGOS]
 
     def _generate_password(self, payout_currency):
         return "c=%s" % (payout_currency.upper())
 
-    def _create_miners_for_currencies(self, currencies):
+    def _create_miners_for_algo_info(self, algo_info):
         LOG.debug("Prepping miners...")
         miners = {}
-        algo_to_port = {c["algo"]: c["port"] for c in currencies}
+        algo_to_port = {a.algo: a.port for a in algo_info}
         supported_algos = set(algo_to_port.keys())
 
         # CCMiner
@@ -164,14 +226,25 @@ class ZPoolMiningGroup(AbstractMiningGroup):
         LOG.debug("Loading benchmarks from cache...")
         return {algo: miner.benchmark() for algo, miner in algo_to_miners.items()}
 
-    def get_most_profitable_miner(self):
-        currencies = self._get_currencies()
-        currencies = self._filter_blacklisted_algos_from_currencies(currencies)
-        algo_to_miners = self._create_miners_for_currencies(currencies)
-        algo_to_benchmarks = self._get_benchmarks(algo_to_miners)
-        best_algo = self._fetch_most_profitable_algo(currencies, algo_to_benchmarks)
+    def _get_most_profitable_miner_for_source(self, source):
+        LOG.debug("Finding most profitable algo for source \"%s\"..." % source)
+        if source == "algo":
+            algo_info = self._get_algo_info_from_currencies()
+        elif source == "currency":
+            algo_info = self._get_algo_info_from_statuses()
 
+        algo_info = self._filter_blacklisted_algos_from_algo_info(algo_info)
+        algo_to_miners = self._create_miners_for_algo_info(algo_info)
+        algo_to_benchmarks = self._get_benchmarks(algo_to_miners)
+        best_algo = self._fetch_most_profitable_algo(algo_info, algo_to_benchmarks)
         return algo_to_miners[best_algo]
+
+    def get_most_profitable_miner(self):
+        if self._calc_all_algo_data_sources:
+            other_source = "currency" if self._algo_data_source == "algo" else "algo"
+            self._get_most_profitable_miner_for_source(other_source)
+
+        return self._get_most_profitable_miner_for_source(self._algo_data_source)
 
     def __str__(self):
         return "Zpool Mining Group"
