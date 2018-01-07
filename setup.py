fa8447be131501d28b829d2560a048d950b5e745
Bryan Jadot <bryan.jadot@gmail.com>
2018-01-06 20:47:38 -0800

    Add basic unittest support

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
-
 from setuptools import setup, find_packages
 setup(
     name="SpeedMiner",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
+    test_suite="tests",
     entry_points={
         'console_scripts': [
             'miner = speed_miner.miner:start',
---
+++ tests/test_miner.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestMiner(TestCase):
+
+    def test_miners(self):
+        self.assertEqual("a", "a")
