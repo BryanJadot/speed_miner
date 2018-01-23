b0486f16f36fa55659b62040e7b6c3841e0cadcd
Bryan Jadot <bryan.jadot@gmail.com>
2018-01-23 00:12:53 -0800

    Speed up termination

--- speed_miner/misc/process_util.py
+++ speed_miner/misc/process_util.py
@@ -2,21 +2,26 @@ from subprocess import PIPE, Popen, TimeoutExpired
 
 from speed_miner.misc.logging import LOG
 
+
 procs = []
 
 
 def start_proc(cmd, pipe_stdout=False):
+    global pid_to_procs
     kwargs = {}
     if pipe_stdout:
         kwargs["stdout"] = PIPE
 
     p = Popen(cmd.split(" "), **kwargs)
+
     procs.append(p)
+
     LOG.debug("Started \"%s\" with pid of %i", cmd, p.pid)
     return p
 
 
 def term_proc(proc, term_wait_time=3):
+    global pid_to_procs
     if proc.poll() is None:
         LOG.debug(
             "Terminating process %i and waiting up to %i seconds for it to end...",
@@ -37,12 +42,14 @@ def term_proc(proc, term_wait_time=3):
         proc.kill()
         proc.wait()
 
+    assert proc.poll() is not None, "Proc %i didn't terminate properly" % proc.pid
+
     LOG.debug("Process %i terminated with %i", proc.pid, proc.poll())
 
 
 def term_all_procs():
+    global procs
     LOG.debug("Terminating all processes...")
     for p in procs:
-        LOG.debug("Reviewing a process with exit status %s..." % p.poll())
         if p.poll() is None:
             term_proc(p, term_wait_time=1)
